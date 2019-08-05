7b26df04d3b953fade4f24a5129bbc01578ba6b1
Matthew Malone <matthewmalon@umass.edu>
2019-08-05 19:03:00 -0400

    favicon and analytics

--- data/config.js
+++ data/config.js
@@ -1,25 +1,18 @@
 module.exports = {
   defaultTitle: 'Matt Malone',
-  logo: 'https://portfolio.smakosh.com/favicon/favicon-512.png',
+  logo: 'https://mattmalone.dev/favicon/favicon.png',
   author: 'Matt Malone',
-  url: 'https://portfolio.smakosh.com',
+  url: 'https://mattmalone.dev',
   legalName: 'Matthew Malone',
-  defaultDescription: 'I’m Matt and I’m a Backend & Devops engineer!',
+  defaultDescription: 'I’m Matt and I’m Software Engineer!',
   socialLinks: {
     github: 'https://github.com/mattm-malone',
     linkedin: 'https://www.linkedin.com/in/mattm-malone/',
   },
   themeColor: '#4285f4',
   backgroundColor: '#4285f4',
-  address: {
-    city: 'City',
-    region: 'Region',
-    country: 'Country',
-    zipCode: 'ZipCode',
-  },
   contact: {
-    email: 'mattmmalone@gmail.com',
+    email: 'hey@mattmalone.dev',
   },
   foundingDate: '2019',
-  // recaptcha_key: '6Lcs6lQUAAAAAEwhNH2IsobIe2csdda4TU3efpMN'
 }
